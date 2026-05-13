0238d6e33314dd3a036e9cfaec4be179e892f575
AGkey-tms <agkeyebay@gmail.com>
2026-05-14 01:32:44 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -31,6 +31,7 @@ const port = process.env.PORT || 3000;
 const mongoUri = process.env.MONGODB_URI;
 
 app.use(cors());
+app.use(express.static(process.cwd()));
 app.use(express.json({ type: "application/json" }));
 
 app.get("/health", (req, res) => {
